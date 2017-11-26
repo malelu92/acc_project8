9c1b5b3ec4031d3e2f8ce10870f27708fb12d248
Marina Leao Lucena <marinaleaolucena@Marinas-MBP.fios-router.home>
2017-11-26 16:20:13 -0500

    adding elements

---
+++ speech_rec.js
@@ -0,0 +1,16 @@
+function startButton(event) {
+  if (recognizing) {
+    recognition.stop();
+    return;
+  }
+  final_transcript = '';
+  recognition.lang = select_dialect.value;
+  recognition.start();
+  ignore_onend = false;
+  final_span.innerHTML = '';
+  interim_span.innerHTML = '';
+  start_img.src = 'mic-slash.gif';
+  showInfo('info_allow');
+  showButtons('none');
+  start_timestamp = event.timeStamp;
+}
